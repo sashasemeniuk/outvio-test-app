4ed4ed6ec603388d591cf30f36a4f6bf84bd587c
Oleksandr Semeniuk <o.semeniuk31@gmail.com>
2021-03-01 10:19:28 +0200

    Create basic layout

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,31 @@
+import React from 'react';
+
 import './style.sass';
+import { Dropdown, Container, Tab, Card } from 'semantic-ui-react';
 
 export function App() {
-  return <div className="app"></div>;
+  const panes = [
+    {
+      menuItem: 'Tab 1',
+      render: () => <Tab.Pane>Tab 1 Content</Tab.Pane>,
+    },
+    {
+      menuItem: 'Tab 2',
+      render: () => <Tab.Pane>Tab 2 Content</Tab.Pane>,
+    },
+  ];
+
+  return (
+    <div className="app">
+      <Container text>
+        <Card fluid>
+          <Card.Content>
+            <Dropdown placeholder="Select Country" fluid search selection />
+            <Tab panes={panes} />
+          </Card.Content>
+          <Card.Content extra>Chart controls here</Card.Content>
+        </Card>
+      </Container>
+    </div>
+  );
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.sass';
+import 'semantic-ui-css/semantic.min.css';
 
 import { App } from './components/';
 
